3b0a074b82e87298eefd790ade1726ffb40d19a3
Houda Jawad <houdajawad@gmail.com>
2022-12-14 15:32:19 -0800

    Print a for statement

---
+++ test1.js
@@ -0,0 +1,6 @@
+var names = ["pilar", "petunia", "pamela", "tan", "amanda", "phil"];
+for(var i = 0; i < names.length; i++) {
+    if (names[i].startsWith("p")){
+        console.log(names[i])
+    }
+}
